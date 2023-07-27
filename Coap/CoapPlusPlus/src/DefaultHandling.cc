7bac2e86922f0f77ec1b5e50e9c55703b720fb03
HuluMan <491849417@qq.com>
2023-07-27 16:28:21 +0800

    style: 默认处理器添加更多打印

--- Coap/CoapPlusPlus/src/DefaultHandling.cc
+++ Coap/CoapPlusPlus/src/DefaultHandling.cc
@@ -3,6 +3,8 @@
 #include "coap/DataStruct/BinaryConst.h"
 #include "coap/Pdu/RequestPdu.h"
 #include "coap/Pdu/ResponsePdu.h"
+#include "coap/Log.h"
+#include <iostream>
 
 namespace CoapPlusPlus {
 
@@ -11,14 +13,18 @@ SendersManager::DefaultHandling::DefaultHandling(BinaryConst token) noexcept
 
 bool SendersManager::DefaultHandling::onAck(Session &session, const RequestPdu *request, const ResponsePdu *response) noexcept
 {
+    Log::Logging(LOG_LEVEL::INFO, "The corresponding Handling is not found for onAck, use the DefaultHandling instead.\n");
     Pdu::LogPdu(LOG_LEVEL::INFO, request);
     Pdu::LogPdu(LOG_LEVEL::INFO, response);
+    std::flush(std::cout);
     return true;
 }
 
 void SendersManager::DefaultHandling::onNAck(Session &session, RequestPdu request, NAckReason reason) noexcept
 {
+    Log::Logging(LOG_LEVEL::INFO, "The corresponding Handling is not found for onNAck, use the DefaultHandling instead.\n");
     Pdu::LogPdu(LOG_LEVEL::INFO, &request);
+    std::flush(std::cout);
 }
 
 }; // namespace CoapPlusPlus
